be64a3a348f894c9d8b9977cc54393807a0c35a2
BZR4 <BZR4@users.noreply.github.com>
2025-08-27 19:55:13 -0300

    Update 1

--- src/domain/Pikachu.java
+++ src/domain/Pikachu.java
@@ -5,8 +5,40 @@ public class Pikachu {
     private int cp;
     private int hp;
 
+    public void setHp(int hp) {
+        if (hp > 0) {
+            this.hp = hp;
+        } else {
+            System.out.println("Valor inválido de hp: " + hp);
+        }
+    }
+
     public int getHp() {
         return hp;
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        if (nick != null && nick.length() > 3) {
+            this.nick = nick;
+        } else {
+            System.out.println("Valor inválido de nick: " + nick);
+        }
+    }
+
+    public int getCp() {
+        return cp;
+    }
+
+    public void setCp(int cp) {
+        if (hp > 0) {
+            this.cp = cp;
+        } else {
+            System.out.println("Valor inválido de cp: " + cp);
+        }
     }
 
     public void thunderbolt() {
