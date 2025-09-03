d212812534f4a45208d3c0a027b615079c3e8caf
BZR4 <BZR4@users.noreply.github.com>
2025-09-03 20:25:04 -0300

    Adicao de //TODOs:

--- src/App.java
+++ src/App.java
@@ -5,11 +5,29 @@ import java.util.Arrays;
 public class App {
     public static void main(String[] args) throws Exception {
         Curso c1 = new Curso();
-        c1.setNome("DSM");
-        c1.setConhecimentos(Arrays.asList("A", "B", "C"));
+        c1.setNome("Desenvolviento de Software Multiplataforma");
+        c1.setConhecimentos(Arrays.asList(
+                "Programação Web",
+                "Programação Mobile",
+                "Integração Contínua/Entrega Contínuo",
+                "Cloud Computing")
+        );
 
-        Curso c2 = new Curso();
-        c2.setNome("DSM");
-        c2.setConhecimentos(Arrays.asList("A", "B", "C"));
+        //TODO: Criar alunos
+
+        //TODO: Criar professor
+
+        //TODO: Criar relacionamentos e ajustar dependencias
+
+        //TODO: Testar o código
+
+        //TODO: Criar Departamentos
+
+        //TODO: Criar Universidade
+
+        //TODO: Criar relacionamentos e ajustar dependencias
+
+        //TODO: Testar o código
+
     }
 }
